517e6d99e7407593de3b2f337dcaef99b4e88eed
Jonah Onah <onahjonah@gmail.com>
2026-04-13 22:11:45 +0100

    fixed error on pymysql

--- config/settings/production.py
+++ config/settings/production.py
@@ -28,6 +28,10 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT', default='3306', cast=int),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
