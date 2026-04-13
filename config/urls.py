4a7f38d70637871c960f515e74f9f98a2d16cc93
Jonah Onah <onahjonah@gmail.com>
2026-04-13 22:30:45 +0100

    fixed error on pymysql

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from config.views import redirect_to_frontend
 
 urlpatterns = [
+    path('', redirect_to_frontend, name='redirect_to_frontend'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
---
+++ config/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import redirect
+
+def redirect_to_frontend(request):
+    return redirect("https://web.middlebeltmemorial.org")
